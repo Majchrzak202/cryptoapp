69a31a6db7e033c89ed308743d7ea5a2e9f95893
MarcinMaster <m.luba505@gmail.com>
2022-08-22 15:54:52 +0200

    Added a Context to manage Firebase Auth and all it utils

---
+++ src/context/UserAuthContextProvider.js
@@ -0,0 +1,51 @@
+import React, { useContext, createContext, useState, useEffect } from "react";
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+} from "@firebase/auth";
+import auth from "../firebase/firebase";
+
+const userAuthContext = createContext();
+
+export function UserAuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  const Login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const SignUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const Logout = () => {
+    return signOut(auth);
+  };
+
+  const ResetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(user);
+    });
+  }, [user]);
+
+  return (
+    <userAuthContext.Provider
+      value={{ Login, Logout, SignUp, ResetPassword, user }}
+    >
+      {children}
+    </userAuthContext.Provider>
+  );
+}
+
+export default UserAuthContextProvider;
+export function useAuth() {
+  return useContext(userAuthContext);
+}
