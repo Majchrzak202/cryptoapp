4bde587f9bfa2745d93737826632ab49034100aa
MarcinMaster <m.luba505@gmail.com>
2022-09-01 09:02:42 +0200

    Minor code changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import HomePage from "./screens/HomePage";
-import CryptoPage from "./screens/CryptoPage";
-import "./App.css";
-import { Routes, Route } from "react-router";
-import Login from "./screens/Login";
-import Portfolio from "./screens/Portfolio";
-import CoinPage from "./components/coins/coin-page/CoinPage";
-import About from "./screens/About";
-import Contact from "./screens/Contact";
+import BaseRoutes from "./routes/BaseRoutes";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cryptos" element={<CryptoPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/coinpage/:id" element={<CoinPage />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+      <BaseRoutes />
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import UserAuthContextProvider from "./context/UserAuthContextProvider";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import PortfolioContextProvider from "./context/PortfolioContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <UserAuthContextProvider>
+        <PortfolioContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PortfolioContextProvider>
+      </UserAuthContextProvider>
+    </Provider>
   </React.StrictMode>
 );
