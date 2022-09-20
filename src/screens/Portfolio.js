b55199415d2ab7044b2e10f32dad068337d7ee9e
MarcinMaster <m.luba505@gmail.com>
2022-09-20 09:41:55 +0200

    Added a padding into Portoflio

--- src/screens/Portfolio.js
+++ src/screens/Portfolio.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Box, Typography } from "@mui/material";
 import CoinCard from "../components/coins/coin-card/CoinCard";
-import { Link } from "react-router-dom";
 import { Skeleton } from "@mui/material";
+import { getDatabase, ref, remove } from "firebase/database";
+import { useAuth } from "../context/UserAuthContextProvider";
 
 const useStyles = makeStyles({
   portfolio: {
@@ -17,18 +18,24 @@ const useStyles = makeStyles({
 
 const Portfolio = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
   const classes = useStyles();
+  const {user} = useAuth()
 
   useEffect(() => {
     fetchCoinsFromDatabase();
   }, []);
 
+   if (user === null) {
+    return null
+  } 
+
   const fetchCoinsFromDatabase = async () => {
+    setLoading(true);
     const response = await fetch(
-      "https://cryptocurrency-project-b4951-default-rtdb.firebaseio.com/crypto.json"
+      `https://cryptocurrency-project-b4951-default-rtdb.firebaseio.com/${user.uid}.json`
     );
     const data = await response.json();
-    console.log(data);
     const loadedCoins = [];
 
     for (const key in data) {
@@ -43,10 +50,18 @@ const Portfolio = () => {
       });
     }
     setCoins(loadedCoins);
-    console.log(loadedCoins);
+    /* console.log(loadedCoins); */
+    setLoading(false);
   };
 
-  if (coins.length === 0) {
+  const removeHandler = async (coinName) => {
+    const db = getDatabase();
+    const data = await remove(ref(db, `${user.uid}/` + coinName))
+    console.log(db);
+    fetchCoinsFromDatabase()
+  };
+
+  if (loading) {
     return (
       <Box
         style={{
@@ -68,25 +83,22 @@ const Portfolio = () => {
 
   return (
     <div className={classes.portfolio}>
-      <Box sx={{ flexGrow: 1 }}>
-        <Typography variant="h4" textAlign="center">
-          This is youre Portfolio fren
-        </Typography>
-        <Grid sx={{ marginTop: "15px" }} container spacing={1} height="100%">
-          {coins.map((coin) => (
-            <Grid
-              xs={12}
-              sm={6}
-              md={4}
-              lg={2}
-              key={coin.name}
-              height="100%"
-              item
-            >
-              {/* <Link
-                style={{ textDecoration: "none" }}
-                to={`/coinpage/${coin.id}`}
-              > */}
+      {coins ? (
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="h4" textAlign="center">
+            This is youre Portfolio fren
+          </Typography>
+          <Grid sx={{ marginTop: "15px" }} container spacing={1} height="100%">
+            {coins.map((coin) => (
+              <Grid
+                xs={12}
+                sm={6}
+                md={4}
+                lg={2}
+                key={coin.name}
+                height="100%"
+                item
+              >
                 <CoinCard
                   id={coin.id}
                   name={coin.name}
@@ -96,12 +108,24 @@ const Portfolio = () => {
                   priceChangeHour={coin.priceChangeHour}
                   priceChangeDay={coin.priceChangeDay}
                   priceChangeWeek={coin.priceChangeWeek}
+                  removeHandler={removeHandler}
                 />
-            {/*   </Link> */}
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      ) : (
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+          }}
+        >
+          No coins, please add some!
+        </Box>
+      )}
     </div>
   );
 };
