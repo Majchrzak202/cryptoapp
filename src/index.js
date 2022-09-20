12c2b0f03f065ad6dcb60f022d1e7024a063e900
MarcinMaster <m.luba505@gmail.com>
2022-09-20 09:42:59 +0200

    Removed REDUX provider

--- src/index.js
+++ src/index.js
@@ -4,24 +4,20 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import UserAuthContextProvider from "./context/UserAuthContextProvider";
-import { Provider } from "react-redux";
-import store from "./store/store";
 import PortfolioContextProvider from "./context/PortfolioContextProvider";
 import { ThemeContextProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeContextProvider>
-        <UserAuthContextProvider>
-          <PortfolioContextProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </PortfolioContextProvider>
-        </UserAuthContextProvider>
-      </ThemeContextProvider>
-    </Provider>
+    <ThemeContextProvider>
+      <UserAuthContextProvider>
+        <PortfolioContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PortfolioContextProvider>
+      </UserAuthContextProvider>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
