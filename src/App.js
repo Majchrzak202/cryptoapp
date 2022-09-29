4d8d2cb9202a3ef241645cd0cf1bf3742c493c7d
MarcinMaster <m.luba505@gmail.com>
2022-09-29 17:43:00 +0200

    Little changes in styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import BaseRoutes from "./routes/BaseRoutes";
+import "./App.css";
+import { useTheme } from "./context/ThemeContext";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="app">
+    <div className="App" id={theme}>
       <Navbar />
+      <ScrollToTop />
       <BaseRoutes />
       <Footer />
     </div>
