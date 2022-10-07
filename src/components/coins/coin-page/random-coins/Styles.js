afa9f9adceff61f882c8bbb8957d9cbba878f5c7
MarcinMaster <m.luba505@gmail.com>
2022-10-07 13:22:24 +0200

    Added random Coin components with styling

---
+++ src/components/coins/coin-page/random-coins/Styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  randomCoinBox: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "40px",
+  },
+  newsBox: {
+    backgroundColor: "#fff6e9",
+    width: "33%",
+    marginLeft: 10,
+    padding: 10,
+    borderRadius: 8,
+    height: 410,
+  },
+});
+
+export default useStyles;
