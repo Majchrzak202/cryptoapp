9d6555bc39a01a55b4a65263a51f662b9e1b4f95
MarcinMaster <m.luba505@gmail.com>
2022-10-08 06:48:32 +0200

    Moved main into separate directory, added Styles.js file for styling, removed old node

---
+++ src/screens/main/Main.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import useStyles from "./Styles";
+import TitleVideo from "./../../assets/TitleVideo.mp4";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Link } from "react-scroll";
+
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.main}>
+      <div className={classes.videoDiv}>
+        <video className={classes.video} src={TitleVideo} autoPlay loop muted />
+      </div>
+      <Box className={classes.absoluteText}>
+        <Box>
+          <Typography className={classes.text}>Coin Sauce</Typography>
+          <Typography className={classes.secondText}>
+            Crypto Tracker App
+            <Link
+              to="mainMenu"
+              spy={true}
+              smooth={true}
+              offset={-30}
+              duration={500}
+            >
+              <KeyboardArrowDownIcon
+                fontSize="large"
+                className={classes.arrowIcon}
+              />
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Main;
