b7349139feec728ba119b2d3b90d47e4bb571e23
MarcinMaster <m.luba505@gmail.com>
2022-10-08 05:55:07 +0200

    Adjusted the styling of About component. Added smoth links and Router Link. Adjusted styling on wider screens. Added makeStyles

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,34 +1,86 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import cryptoLogo from "./../assets/cryptoLogo.png";
+import { Typography, Box } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { Link as SmoothLink } from "react-scroll";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   about: {
+    width: "100vw",
     display: "flex",
-    flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
-    minHeight: "100vh",
-    marginTop: "-50px",
+    height: "100vh",
+    paddingTop: 80,
+    [theme.breakpoints.up(2400)]: {
+      height: "50vh",
+    },
   },
   img: {
-    width: "200px",
-    height: "200px",
+    width: "290px",
+    height: "120px",
+    marginRight: 8,
   },
   title: {
     fontFamily: "Roboto",
     fontSize: "28px",
   },
-});
+  link: {
+    color: "#e67b54",
+    textDecoration: "none",
+    fontWeight: 500,
+    cursor: 'pointer'
+  },
+}));
 
 const About = () => {
   const classes = useStyles();
   return (
-    <div className={classes.about}>
-      <img className={classes.img} alt="crypto" src={cryptoLogo} />
-      <h4 className={classes.title}>
-        This is an educational Project <br></br> made with Love and React!
-      </h4>
+    <div className={classes.about} id="about">
+      <Box style={{ width: 1536, padding: 50 }}>
+        <Typography
+          style={{
+            fontFamily: "Roboto",
+            fontSize: "35px",
+            textAlign: "left",
+            paddingBottom: 10,
+          }}
+        >
+          About coin sauce
+        </Typography>
+        <Typography
+          style={{
+            fontFamily: "Roboto",
+            fontSize: "15px",
+            textAlign: "left",
+          }}
+        >
+          This is an educational Project made with Love and React!<br></br>
+          You can easily see prices of{" "}
+          <Link className={classes.link} to="/cryptos">
+            top 100 cryptocurrencies
+          </Link>
+          <br></br>
+          Track them and save into portfolio (you have to{" "}
+          <Link className={classes.link} to="/signup">
+            create account
+          </Link>
+          )<br></br>
+          Feel free to{" "}
+          <SmoothLink
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-30}
+            duration={500}
+            className={classes.link}
+          >
+            {" "}
+            contact author
+          </SmoothLink>
+          <br></br>
+          All right reserved
+        </Typography>
+      </Box>
     </div>
   );
 };
