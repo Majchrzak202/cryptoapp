79320badff541239e371f8d5a082ccf5db6dc2eb
MarcinMaster <m.luba505@gmail.com>
2022-10-09 15:38:55 +0200

    Added a Chart.js to display price info data. Added simple styling and Dayjs as a lib to transofrom unix date format

---
+++ src/components/coins/coin-page/chart/Chart.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import dayjs from "dayjs";
+import { useParams } from "react-router";
+import { PolarArea } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+const Chart = () => {
+  const [chartData, setChartData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      const response = await fetch(
+        `https://api.coinstats.app/public/v1/charts?period=1m&coinId=${id}`
+      );
+      const data = await response.json();
+      setChartData(
+        data.chart.map((value) => ({ x: value[0], y: value[1].toFixed(2) }))
+      );
+    };
+    fetchChartData();
+  }, [id]);
+
+  if (chartData.length === 0) {
+    return null;
+  }
+
+  const options = {
+    responsive: true,
+    elements: {
+      point: {
+        borderWidth: 0.5,
+        radius: 0.5,
+        backgroundColor: "rgba(0,0,0,0)",
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        grid: {
+          display: false
+        }
+      }
+    }
+  };
+
+  const data = {
+    labels: chartData.map((value) => dayjs.unix(value.x).format("MMM DD")),
+    datasets: [
+      {
+        fill: true,
+        label: `${id} price USD`,
+        data: chartData.map((val) => val.y),
+        borderColor: "rgba(180, 206, 241, 255)",
+        backgroundColor: "rgba(180, 206, 241, 0.2)",
+        borderWidth: 2.5,
+       
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  );
+};
+
+export default Chart;
