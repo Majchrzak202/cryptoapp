deaf7be4f54750b4282cb99545c9336ec88cec53
MarcinMaster <m.luba505@gmail.com>
2022-10-09 17:16:10 +0200

    Minor code changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import BaseRoutes from "./routes/BaseRoutes";
 import "./App.css";
 import { useTheme } from "./context/ThemeContext";
 import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
-
+import { Suspense } from "react";
 function App() {
   const { theme } = useTheme();
   return (
     <div className="App" id={theme}>
       <Navbar />
-
       <ScrollToTop />
-      <BaseRoutes />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BaseRoutes />
+      </Suspense>
       <Footer />
     </div>
   );
