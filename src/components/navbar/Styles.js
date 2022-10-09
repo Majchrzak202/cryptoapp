c4bec62017aeb7bd508d6003cd075fcf7ed5446d
MarcinMaster <m.luba505@gmail.com>
2022-10-09 16:32:37 +0200

    Some minor code changes

--- src/components/navbar/Styles.js
+++ src/components/navbar/Styles.js
@@ -3,19 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   appbar: {
     "& #Light": {
-      backgroundColor: "rgba(42,52,65,255)      ",
+      backgroundColor: "rgba(1,10,21,255)",
     },
     "& #Dark": {
-      backgroundColor: "rgba(27, 31, 36, 255)",
+      backgroundColor: "rgba(1,10,21,255)",
     },
   },
   a: {
     color: "red",
     textDecoration: "none",
-    textDecorationColor: "white",
+    textDecorationColor: "rgba(180, 206, 241, 255)",
   },
   link: {
-    color: "white",
+    color: "rgba(180, 206, 241, 255)",
     textDecoration: "none",
     [theme.breakpoints.down("xs")]: {
       color: "black",
