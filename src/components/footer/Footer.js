6a1e8b005a619afc58a1039dd4ab7c46c79c41d9
MarcinMaster <m.luba505@gmail.com>
2022-10-09 15:59:27 +0200

    Moved styling into separate Styles file

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import useStyles from "./Styles";
+import { useTheme } from "../../context/ThemeContext";
 
 const Footer = () => {
+  const classes = useStyles();
+  const { theme } = useTheme();
   return (
-    <div className='footer'><p>Footer</p></div>
-  )
-}
+    <div className={classes.root}>
+      <Box id={theme} className={classes.main}>
+        <Typography>Copryright 2022</Typography>
+      </Box>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
