7387713c506c59c8a7cc7afa7fcb37df6f91cc32
MarcinMaster <m.luba505@gmail.com>
2022-10-12 05:52:43 +0200

    Adjusted width and fontSize

--- src/screens/about/About.js
+++ src/screens/about/About.js
@@ -9,23 +9,8 @@ const About = () => {
   return (
     <div className={classes.about} id="about">
       <Box style={{ width: 1536, padding: 50 }}>
-        <Typography
-          style={{
-            fontFamily: "Roboto",
-            fontSize: "35px",
-            textAlign: "left",
-            paddingBottom: 10,
-          }}
-        >
-          About coin sauce
-        </Typography>
-        <Typography
-          style={{
-            fontFamily: "Roboto",
-            fontSize: "15px",
-            textAlign: "left",
-          }}
-        >
+        <Typography className={classes.mainTitle}>About coin sauce</Typography>
+        <Typography className={classes.subtitle}>
           This is an educational Project made with Love and React!<br></br>
           You can easily see prices of{" "}
           <Link className={classes.link} to="/cryptos">
--- src/screens/about/Styles.js
+++ src/screens/about/Styles.js
@@ -7,18 +7,42 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     height: "100vh",
     paddingTop: 80,
+    [theme.breakpoints.up(1200)]: {
+      alignItems: "flex-start",
+      justifyContent: "center",
+    },
+    [theme.breakpoints.up(1500)]: {
+      height: "80vh",
+    },
+    [theme.breakpoints.up(2000)]: {
+      height: "70vh",
+    },
     [theme.breakpoints.up(2400)]: {
       height: "50vh",
     },
   },
-  img: {
-    width: "290px",
-    height: "120px",
-    marginRight: 8,
-  },
-  title: {
+  mainTitle: {
     fontFamily: "Roboto",
-    fontSize: "28px",
+    fontSize: 35,
+    textAlign: "center",
+    paddingBottom: 10,
+    [theme.breakpoints.up(2000)]: {
+      fontSize: 60,
+    },
+    [theme.breakpoints.down(400)]: {
+      fontSize: 30,
+      textAlign: "left",
+    },
+  },
+
+  subtitle: {
+    fontFamily: "Roboto",
+    fontSize: 18,
+    textAlign: "center",
+    [theme.breakpoints.down(400)]: {
+      fontSize: 15,
+      textAlign: "left",
+    },
   },
   link: {
     color: "#e67b54",
