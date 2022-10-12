52fa0a73f750ba9f3412a0fac08ba71ab455717a
MarcinMaster <m.luba505@gmail.com>
2022-10-12 05:53:35 +0200

    Final styling adjustment

--- src/screens/contact/Styles.js
+++ src/screens/contact/Styles.js
@@ -1,7 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  root: { backgroundColor: "rgba(1,10,21,255)" },
+  root: {
+    backgroundColor: "rgba(1,10,21,255)",
+    [theme.breakpoints.up(1200)]: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
   container: {
     marginBottom: 50,
     paddingTop: 30,
@@ -10,8 +16,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "flex-start",
     minHeight: "100vh",
     paddingRight: 50,
-
     width: "auto",
+    [theme.breakpoints.up(2400)]: {
+      height: "50vh",
+    },
     [theme.breakpoints.up(2000)]: {
       minWidth: 1500,
       paddingTop: 100,
@@ -20,7 +28,11 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up(1500)]: {
       minWidth: 1500,
       paddingTop: 100,
-      minHeight: "70vh",
+      minHeight: "80vh",
+    },
+    [theme.breakpoints.up(1000)]: {
+      alignItems: "center",
+      justifyContent: "center",
     },
     [theme.breakpoints.down(1500)]: {
       paddingTop: 80,
@@ -45,6 +57,14 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+  },
+  mainTitle: {
+    fontSize: 30,
+    fontFamily: "Roboto",
+    fontWeight: 800,
+    [theme.breakpoints.up(2000)]: {
+      fontSize: 60
+     },
   },
 }));
 
