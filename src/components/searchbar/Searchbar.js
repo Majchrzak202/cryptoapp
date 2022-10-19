8743c679b4b0abd42f1b9a2f188090abaa65115a
MarcinMaster <m.luba505@gmail.com>
2022-10-19 10:54:29 +0200

    Added a styledTextField to change colors

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { TextField, Box } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
+import { styled } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   inputLabel: {
@@ -8,20 +9,45 @@ const useStyles = makeStyles({
   },
 });
 
+const StyledTextField = styled(TextField)({
+  "& label": {
+    color: "rgba(180, 206, 241, 255)",
+  },
+  "&:hover label": {
+    fontWeight: 700,
+  },
+  "& label.Mui-focused": {
+    color: "rgba(180, 206, 241, 255)",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "rgba(180, 206, 241, 255)",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "rgba(180, 206, 241, 255)",
+    },
+    "&:hover fieldset": {
+      borderColor: "rgba(180, 206, 241, 255)",
+      borderWidth: 2,
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "rgba(180, 206, 241, 255)",
+    },
+  },
+});
+
 const Searchbar = ({ setSearch }) => {
   const classes = useStyles();
   return (
     <Box sx={{ marginBottom: "15px" }}>
-      <TextField
-        InputLabelProps={{ style: { color: "white" } }}
+      <StyledTextField
         sx={{
           input: { color: "white" },
-          formControl: { color: "white" },
         }}
         onChange={(e) => setSearch(e.target.value)}
         id="standard-basic"
         label="Search Coin"
-        variant="standard"
+        variant="outlined"
       />
     </Box>
   );
