eed06344ec03af008e52e99e5044642b01938f2e
MarcinMaster <m.luba505@gmail.com>
2022-10-27 15:58:18 +0200

    Added framer motion and little styling refactoring

--- src/screens/main-menu/MainMenu.js
+++ src/screens/main-menu/MainMenu.js
@@ -5,7 +5,8 @@ import { motion } from "framer-motion";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { Link as SmoothLink } from "react-scroll";
 import useStyles from "./Styles";
-import cryptoLogo from "./../../assets/cryptoLogo.svg";
+/* import cryptoLogo from "./../../assets/cryptoLogo.svg"; */
+import { useInView } from "react-intersection-observer";
 
 const glowVariants = {
   initial: {
@@ -18,71 +19,78 @@ const glowVariants = {
 
 const MainMenu = () => {
   const classes = useStyles();
+  const [ref, inView] = useInView();
   return (
-    <Box id="mainMenu" className={classes.main}>
-      <Box className={classes.menuBox}>
-        <Typography className={classes.title}>
-          Welcome to the Coin Sauce
-        </Typography>
+    <Box ref={ref} id="mainMenu" className={classes.main}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        animate={inView ? { y: -30, opacity: 1 } : ""}
+        style={{maxWidth: '100vw'}}
+      >
+        <Box className={classes.menuBox}>
+          <Typography className={classes.title}>
+            Welcome to the Coin Sauce
+          </Typography>
 
-        <motion.div
-          initial="initial"
-          whileHover="hover"
-          className={classes.motionMain}
-        >
-          <Box className={classes.menuItemBox}>
-            <motion.div variants={glowVariants}>
-              <ArrowRightIcon className={classes.arrow} />
-            </motion.div>
-            <Link className={classes.menuItemLink} to="/cryptos">
-              <Typography className={classes.menuItemTitle}>
-                Search coins
-              </Typography>
-            </Link>
-          </Box>
-        </motion.div>
-
-        <motion.div
-          initial="initial"
-          whileHover="hover"
-          className={classes.motionMain}
-        >
-          <Box className={classes.menuItemBox}>
-            <motion.div variants={glowVariants}>
-              <ArrowRightIcon className={classes.arrow} />
-            </motion.div>
-            <Typography className={classes.menuItemTitle}>
-              <Link className={classes.menuItemLink} to="/signup">
-                Create account
+          <motion.div
+            initial="initial"
+            whileHover="hover"
+            className={classes.motionMain}
+          >
+            <Box className={classes.menuItemBox}>
+              <motion.div variants={glowVariants}>
+                <ArrowRightIcon className={classes.arrow} />
+              </motion.div>
+              <Link className={classes.menuItemLink} to="/cryptos">
+                <Typography className={classes.menuItemTitle}>
+                  Search juicy & hot Coins
+                </Typography>
               </Link>
-            </Typography>
-          </Box>
-        </motion.div>
+            </Box>
+          </motion.div>
 
-        <motion.div
-          initial="initial"
-          whileHover="hover"
-          className={classes.motionMain}
-        >
-          <Box className={classes.menuItemBox}>
-            <motion.div variants={glowVariants}>
-              <ArrowRightIcon className={classes.arrow} />
-            </motion.div>
-            <Typography className={classes.menuItemTitle}>
-              <SmoothLink
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                About Coin Sauce
-              </SmoothLink>
-            </Typography>
-          </Box>
-        </motion.div>
-         <img alt='logo' className={classes.img} src={cryptoLogo} /> 
-      </Box>
+          <motion.div
+            initial="initial"
+            whileHover="hover"
+            className={classes.motionMain}
+          >
+            <Box className={classes.menuItemBox}>
+              <motion.div variants={glowVariants}>
+                <ArrowRightIcon className={classes.arrow} />
+              </motion.div>
+              <Typography className={classes.menuItemTitle}>
+                <Link className={classes.menuItemLink} to="/signup">
+                  Create account
+                </Link>
+              </Typography>
+            </Box>
+          </motion.div>
+
+          <motion.div
+            initial="initial"
+            whileHover="hover"
+            className={classes.motionMain}
+          >
+            <Box className={classes.menuItemBox}>
+              <motion.div variants={glowVariants}>
+                <ArrowRightIcon className={classes.arrow} />
+              </motion.div>
+              <Typography className={classes.menuItemTitle}>
+                <SmoothLink
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  About us
+                </SmoothLink>
+              </Typography>
+            </Box>
+          </motion.div>
+        </Box>
+      </motion.div>
     </Box>
   );
 };
