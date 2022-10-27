bdc09c3844c97238eea20e1f385603ccfa683d69
MarcinMaster <m.luba505@gmail.com>
2022-10-27 15:52:55 +0200

    Added Toastify Container and corresponding CSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import BaseRoutes from "./routes/BaseRoutes";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { useTheme } from "./context/ThemeContext";
 import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 import { Suspense } from "react";
 import LoadingSpinner from "./components/loading-spinner/LoadingSpinner";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const { theme } = useTheme();
@@ -13,7 +15,8 @@ function App() {
     <div className="App" id={theme}>
       <Navbar />
       <ScrollToTop />
-      <Suspense fallback={<LoadingSpinner/>}>
+      <ToastContainer />
+      <Suspense fallback={<LoadingSpinner />}>
         <BaseRoutes />
       </Suspense>
       <Footer />
